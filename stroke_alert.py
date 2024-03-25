5411a1a03856446fe2240cad313934c158bcbd9b
AurelPap <apapillon@aurelpap.fr>
2024-03-25 11:05:41 +0100

    [update] analyse univariée

---
+++ stroke_alert.py
@@ -0,0 +1,40 @@
+# Seuil d'acceptabilité de l'hypothese de correlation
+alpha = 0.05
+
+# Nomination des variables quantitatives
+variables_quantitatives = [
+    'age',
+    'avg_glucose_level',
+    'bmi'
+]
+
+# Liste des couleurs des variables quantitatives
+colors_quantitative = [
+    'midnightblue',
+    'orange',
+    'green'
+]
+
+# Nomination des variables qualitatives
+variables_qualitatives = [
+    'gender',
+    'hypertension',
+    'heart_disease',
+    'ever_married',
+    'work_type',
+    'Residence_type',
+    'smoking_status',
+    'stroke'
+]
+
+# Liste des couleurs des variables qualitatives
+colors_qualitative = [
+    '#FF6F61',
+    '#6B5B95',
+    '#88B04B',
+    '#F7CAC9',
+    '#92A8D1',
+    '#955251',
+    '#B565A7',
+    '#009B77'
+]
